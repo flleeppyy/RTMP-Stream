3da5391712a4bf9c71bc9df94f3a39b6bc6248a6
Flleeppyy <flleeppyy@gmail.com>
2020-08-29 20:47:55 -0700

    Forgot to remove logging

--- middleware/currentlywatching.js
+++ middleware/currentlywatching.js
@@ -3,7 +3,6 @@ const path = require('path')
 
 let init = function (req, res, next) {
     let data = JSON.parse(fs.readFileSync(path.join(__dirname, "../data.json")))
-    console.log(data);
     res.send(data);
 }
 
